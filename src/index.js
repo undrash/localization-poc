bbd04fa969f123e0434b1ffaabbbc392c4a48c3c
Andrei Gaspar <andrei@planeeet.com>
2019-02-19 15:33:24 +0200

    - Translation performed in chunks. Maximum size of a chunk is 100 strings.

--- src/index.js
+++ src/index.js
@@ -11,39 +11,61 @@ const LANGUAGE      = languages.Hungarian;
 
 /**
  * Prepares the string that will be translated
- * @return {string}
+ * @return {string[]}
  */
 function getStringToTranslate() {
 
+    let result = [];
+
     const stringsObj = JSON.parse( fs.readFileSync( "source.json", "utf8" ) );
+
     let strings = [];
 
+    const stringLimit = 100; // Maximum allowed translations per batch
+    let stringCount = 0;
 
     for ( let key in stringsObj ) {
         if ( stringsObj.hasOwnProperty( key ) ) {
             strings.push( '"' + stringsObj[ key ] + '"' );
+
+            stringCount++;
+
+            if ( stringCount >= stringLimit ) {
+                result.push( strings.join( "; " ) );
+                strings = [];
+                stringCount = 0;
+            }
         }
     }
 
-    return strings.join( "; " );
+    if ( stringCount > 0 ) result.push( strings.join( "; " ) );
+
+
+    return result;
 
 }
 
 
 
-/** Get the string */
-const string = getStringToTranslate();
+const translate = async (strings) => {
+
+    let result = "";
+
+    for ( let string of strings ) {
+
+        result += await automation.translate( string, LANGUAGE );
+    }
+
+    writeResult( result );
+};
 
 
 
-/**
- * Perform the translation
- */
-automation.translate( string, LANGUAGE ).then( res => {
+function writeResult(res) {
 
     const stringsObj = JSON.parse( fs.readFileSync( "source.json", "utf8" ) );
 
-    const translatedStrings = res.replace( /['"]+/g, '' ).split( '; ' );
+    const translatedStrings = res.toString().replace( /['"]+/g, '' ).split( '; ' );
 
     for ( let key in stringsObj ) {
         if ( stringsObj.hasOwnProperty( key ) ) {
@@ -54,8 +76,42 @@ automation.translate( string, LANGUAGE ).then( res => {
     fs.writeFileSync( 'translated.json', JSON.stringify( stringsObj, null, 4 ) );
 
     console.log( stringsObj );
+}
 
-});
+
+
+/** Get the string */
+const strings = getStringToTranslate();
+
+/** Get the translations - ASYNC */
+translate( strings )
+    .then( () => console.info( "Translation finished!" ) )
+    .catch( err => console.error( err ) );
+
+
+
+
+
+// /**
+//  * Perform the translation
+//  */
+// automation.translate( string, LANGUAGE ).then( res => {
+//
+//     const stringsObj = JSON.parse( fs.readFileSync( "source.json", "utf8" ) );
+//
+//     const translatedStrings = res.replace( /['"]+/g, '' ).split( '; ' );
+//
+//     for ( let key in stringsObj ) {
+//         if ( stringsObj.hasOwnProperty( key ) ) {
+//             stringsObj[ key ] = translatedStrings[ Object.keys( stringsObj ).indexOf( key ) ];
+//         }
+//     }
+//
+//     fs.writeFileSync( 'translated.json', JSON.stringify( stringsObj, null, 4 ) );
+//
+//     console.log( stringsObj );
+//
+// });
 
 
 
